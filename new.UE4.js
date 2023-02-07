55bd3ff4ab4110579475da1d00865ec17dae54aa
Kostyn2015 <124687102+Kostyn2015@users.noreply.github.com>
2023-02-07 16:08:43 +0300

    Update new.UE4.js

--- new.UE4.js
+++ new.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Kostyn2015/test/fe7009e2eadb8f865000af02404ec251f53aea3b/new.data.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
